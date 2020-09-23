1df961b8fee983dd88be66ca98d455ca4070f32f
Juno Suárez <juno@scroll.com>
2020-09-23 11:52:31 -0700

    Initial Commit

---
+++ AQI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AQI
+//
+//  Created by Juno Suárez on 9/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
