a240ee38b11700dffa8948f1c24979bc3f529bbd
Juno Suárez <juno@scroll.com>
2020-09-23 16:06:23 -0700

    Add click to open map; right click menu to quit

--- AQI/AppDelegate.swift
+++ AQI/AppDelegate.swift
@@ -8,28 +8,62 @@
 import Cocoa
 import SwiftUI
 
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+// EDIT THIS VALUE TO THE SENSOR YOU WANT
+let SENSOR_ID: Int = 43023
 
-    var statusItem: NSStatusItem?
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
+
+    var statusItem: NSStatusItem!
+    var menu: NSMenu!
     var timer: Timer?
     
-    let url = URL(string: "https://www.purpleair.com/json?show=43023")!
-//    let url = URL(string: "http://127.0.0.1:8080/sample.json")!
-    
+//    let url = URL(string: "https://www.purpleair.com/json?show=\(SENSOR_ID)")!
+    let url = URL(string: "http://127.0.0.1:8080/sample.json")!
+    var lat: String?
+    var lon: String?
+    var mapUrl: String?
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        statusItem?.button?.toolTip = "AQI"
+        statusItem.button?.action = #selector(onClick)
+        statusItem.button?.sendAction(on: [.leftMouseUp, .rightMouseUp])
         
-        updateAqi(aqi: nil)
+        menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        menu.delegate = self
+        
+        updateAqi(nil)
         
         timer = Timer.scheduledTimer(timeInterval: 600, target: self, selector: #selector(tick), userInfo: nil, repeats: true)
         timer?.fire()
     }
     
-    func updateAqi(aqi: Int?) {
+    @objc func onClick(sender: NSStatusItem) {
+        
+        let event = NSApp.currentEvent!
+        print("onclick", event.type, NSStatusItem.squareLength, statusItem.statusBar?.thickness)
+        if event.type == NSEvent.EventType.rightMouseUp {
+            // right click, show quit menu
+            statusItem.menu = menu;
+            menu.popUp(positioning: nil,
+                       at: NSPoint(x: 0, y: statusItem.statusBar!.thickness),
+                       in: statusItem.button)
+        } else {
+            // open map in bowser
+            if let mapUrl = mapUrl {
+                if let url = URL(string: mapUrl) {
+                    NSWorkspace.shared.open(url)
+                }
+            }
+        }
+    }
+    @objc func menuDidClose(_ menu: NSMenu) {
+        // remove menu when closed so we can override left click behavior
+        statusItem.menu = nil
+    }
+    
+    func updateAqi(_ aqi: Int?) {
         var text: String
         var color: NSColor
         if let aqi = aqi {
@@ -59,8 +93,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @objc func tick() {
-        print("tick")
-        
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         
@@ -68,61 +100,64 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             if error != nil {
                 return
             }
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+            do {
                 
-                do {
-                    if let obj = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                        let results = obj["results"] as? NSArray
-                        if let results = results {
-                            // each station has 2 sensors. we want to get the average reading between the two sensors
-                            let values = results.compactMap({ (data) -> Float? in
-                                let PM2_5Value = ((data as? NSDictionary)?["PM2_5Value"] as? NSString)?.floatValue
-                                return PM2_5Value
-                            })
-                            let avgPM25 = values.reduce(0.0, +) / Float(values.count)
+                if let data = data,
+                   let obj = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary,
+                   let results = obj["results"] as? NSArray {
+                    print(self.url, results)
+                    // each station has 2 sensors. we want to get the average reading between the two sensors
+                    let values = results.compactMap({ (data) -> Float? in
+                        let PM2_5Value = ((data as? NSDictionary)?["PM2_5Value"] as? NSString)?.floatValue
+                        return PM2_5Value
+                    })
+                    let avgPM25 = values.reduce(0.0, +) / Float(values.count)
+                        
+                    let aqi = self.calculateAqi(pm25: avgPM25)
+                    print("pm25 \(avgPM25), aqi \(String(describing: aqi))")
                             
-                            let aqi = self.calculateAqi(pm25: avgPM25)
-                            print("pm25 \(avgPM25), aqi \(aqi)")
-                            
-                            self.updateAqi(aqi: aqi)
-                        }
+                    // also grab the lat/lon (they're the same for both sensors, so get the first)
+                    if self.mapUrl == nil,
+                       results.count > 0,
+                        let lat = (results[0] as? NSDictionary)?["Lat"] as? Double,
+                        let lon = (results[0] as? NSDictionary)?["Lon"] as? Double {
+                        // make map url
+                        self.mapUrl = "https://www.purpleair.com/map?opt=1/mAQI/a10/cC0#10/\(lat)/\(lon)"
                     }
-                    
-                } catch {
-                    // handle err
+                            
+                    self.updateAqi(aqi)
                 }
+            } catch {
+                // handle err
             }
-            
-            
         }
         task.resume()
-        
     }
 
     func calcAQI(_ Cp: Float, _ Ih: Float, _ Il: Float, _ BPh: Float, _ BPl: Float) -> Int {
-        let a = (Ih - Il);
-        let b = (BPh - BPl);
-        let c = (Cp - BPl);
-        return Int((a/b) * c + Il);
+        let a = (Ih - Il)
+        let b = (BPh - BPl)
+        let c = (Cp - BPl)
+        return Int((a/b) * c + Il)
     }
     
     func calculateAqi(pm25: Float) -> Int? {
         // from https://docs.google.com/document/d/15ijz94dXJ-YAZLi9iZ_RaBwrZ4KtYeCy08goGBwnbCU/edit
         if (pm25 > 350.5) {
-                 return calcAQI(pm25, 500, 401, 500, 350.5);
-               } else if (pm25 > 250.5) {
-                 return calcAQI(pm25, 400, 301, 350.4, 250.5);
-               } else if (pm25 > 150.5) {
-                 return calcAQI(pm25, 300, 201, 250.4, 150.5);
-               } else if (pm25 > 55.5) {
-                 return calcAQI(pm25, 200, 151, 150.4, 55.5);
-               } else if (pm25 > 35.5) {
-                 return calcAQI(pm25, 150, 101, 55.4, 35.5);
-               } else if (pm25 > 12.1) {
-                 return calcAQI(pm25, 100, 51, 35.4, 12.1);
-               } else if (pm25 >= 0) {
-                 return calcAQI(pm25, 50, 0, 12, 0);
-               }
+            return calcAQI(pm25, 500, 401, 500, 350.5)
+        } else if (pm25 > 250.5) {
+            return calcAQI(pm25, 400, 301, 350.4, 250.5)
+        } else if (pm25 > 150.5) {
+            return calcAQI(pm25, 300, 201, 250.4, 150.5)
+        } else if (pm25 > 55.5) {
+            return calcAQI(pm25, 200, 151, 150.4, 55.5)
+        } else if (pm25 > 35.5) {
+            return calcAQI(pm25, 150, 101, 55.4, 35.5)
+        } else if (pm25 > 12.1) {
+            return calcAQI(pm25, 100, 51, 35.4, 12.1)
+        } else if (pm25 >= 0) {
+            return calcAQI(pm25, 50, 0, 12, 0)
+        }
         return nil
     }
     
--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # aqi
+
 mac status bar app to show AQI from purpleair api
 
-It'll refresh the value every 5 minutes.
+It'll refresh the value every 5 minutes. Click the value to open the map.
 
 disclaimer: I have no idea what I'm doing with swift, or air science
 
 Based on the purple air api, see https://docs.google.com/document/d/15ijz94dXJ-YAZLi9iZ_RaBwrZ4KtYeCy08goGBwnbCU/edit
 
-## customizing 
-Find the ID of the monitoring station you want by poking around the network tab of your browser dev tools on the purple air map, eg https://www.purpleair.com/map?opt=1/mAQI/a10/cC0#9.73/45.5249/-122.6215
+## customizing
 
-The station ID should be on a request like https://www.purpleair.com/json?show=43023
+Find the ID of the monitoring station you want by poking around the network tab of your browser dev tools on the purple air map, eg `https://www.purpleair.com/map?opt=1/mAQI/a10/cC0#9.73/45.5249/-122.6215`
 
-Update that URL in the AppDelegate.swift file and rebuild. (I told you I have no idea what I'm doing with swift)
+The station ID should be on a request like `https://www.purpleair.com/json?show=43023`, where 43023 is the ID in this example.
+
+Update the SENSOR_ID constant in the AppDelegate.swift file and rebuild. (I told you I have no idea what I'm doing with swift)
